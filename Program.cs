f1a722891a8b948f0e1087d55701fa8422d0c1fe
Prathamesh Rajput <rajputprathamesh1998@gmail.com>
2020-09-25 10:51:23 +0530

    [Prathamesh Rajput] Refractor. Added UseCase 1 functionality

--- Program.cs
+++ Program.cs
@@ -8,9 +8,16 @@ namespace EmployeeWages
 {
     class Program
     {
+        static bool isPresent;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Problem");
+            isPresent = new Random().Next(0, 2) == 0;
+
+            if (isPresent)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
         }
     }
 }
